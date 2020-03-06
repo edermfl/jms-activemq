25e6be72b84749aa99223201f4f9c816c2daff81
Eder Leite <edermfl@gmail.com>
2020-03-06 08:19:51 -0300

    Consumindo DLQ (Dead Letter Queue)

---
+++ src/main/java/br/com/ederleite/jms/fila/TesteConsumidorDLQ.java
@@ -0,0 +1,30 @@
+package br.com.ederleite.jms.fila;
+
+import javax.jms.*;
+import javax.naming.InitialContext;
+import java.util.Scanner;
+
+public class TesteConsumidorDLQ {
+    public static void main(String[] args) throws Exception {
+	InitialContext context = new InitialContext();
+	ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("ConnectionFactory");
+
+	Connection connection = connectionFactory.createConnection();
+	connection.start();
+
+	Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+	final Destination fila = (Destination) context.lookup("financeiro");
+	MessageConsumer consumer = session.createConsumer(fila);
+
+	consumer.setMessageListener(message -> {
+	    System.out.println("Msg recebida: " + message);
+	});
+
+	new Scanner(System.in).next();
+
+	session.close();
+	connection.close();
+	context.close();
+    }
+}
